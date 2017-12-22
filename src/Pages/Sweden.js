8971d08db9a1f47f51af6a3532e49f6611ed7bd6
Albin Groen <albin.groen@gmail.com>
2017-12-22 14:16:50 +0100

    Fixed some variables

--- src/Pages/Sweden.js
+++ src/Pages/Sweden.js
@@ -10,16 +10,23 @@ export default class Sweden extends React.Component {
     const item4 = 'Electric tape'
     const item5 = 'Computer with baseflight'
     const item6 = 'Power cables'
+
+    const itemAmount1 = '16'
+    const itemAmount1 = '16'
+    const itemAmount1 = '16'
+    const itemAmount1 = '16'
+    const itemAmount1 = '16'
+    const itemAmount1 = '16'
     return (
       <div>
         <Header country="Sweden" />
         <div className="checklist-wrapper">
-          <ChecklistItem ItemTitle={item1} ItemAmount="16" />
-          <ChecklistItem ItemTitle={item2} ItemAmount="16" />
-          <ChecklistItem ItemTitle={item3} ItemAmount="16" />
-          <ChecklistItem ItemTitle={item4} ItemAmount="16" />
-          <ChecklistItem ItemTitle={item5} ItemAmount="16" />
-          <ChecklistItem ItemTitle={item6} ItemAmount="16" />
+          <ChecklistItem ItemTitle={item1} ItemAmount={itemAmount1} />
+          <ChecklistItem ItemTitle={item2} ItemAmount={itemAmount2} />
+          <ChecklistItem ItemTitle={item3} ItemAmount={itemAmount3} />
+          <ChecklistItem ItemTitle={item4} ItemAmount={itemAmount4} />
+          <ChecklistItem ItemTitle={item5} ItemAmount={itemAmount5} />
+          <ChecklistItem ItemTitle={item6} ItemAmount={itemAmount6} />
         </div>
       </div>
     )
